64f1e46b72772a1cc2a9b4075177a8c060f2f741
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-09-06 18:56:13 +0300

    inserts data to the table columns

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "male", "brown", "docile", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "male", "white and black", "docile", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 12, "female", "pure white", "docile", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 6, "male", "white", "docile", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 10, "female", "cream", "docile", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 7, "male", "brown patched", "docile", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 13, "female", "smoke-white", "docile", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, "male", "white-black patched", "docile", 1);
